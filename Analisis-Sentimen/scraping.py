c17b9967bcdeedf8b8e342305fd895c5ed1d1ac7
Khairullah Ilyas <khairullahilyas@gmail.com>
2025-01-20 14:28:07 +0700

    add scraping.py

---
+++ Analisis-Sentimen/scraping.py
@@ -0,0 +1,22 @@
+import csv
+from google_play_scraper import reviews, Sort
+
+# Mengakses ulasan dan informasi aplikasi dari Google Play Store.
+scrapreview, continuation_token = reviews(
+    'com.telkomsel.telkomselcm',    # ID aplikasi
+    lang='id',                 # Bahasa ulasan (default: 'en')
+    country='id',              # Negara (default: 'us')
+    sort=Sort.MOST_RELEVANT,   # Urutan ulasan (default: Sort.MOST_RELEVANT)
+    count=3000,               # Jumlah maksimum ulasan yang ingin diambil
+)
+
+# Menyimpan data ke dalam file CSV
+with open('app_reviews.csv', mode='w', encoding='utf-8', newline='') as file:
+    writer = csv.writer(file)
+
+    # Menulis header (nama kolom)
+    writer.writerow(scrapreview[0].keys())  # Mengambil nama kolom dari data
+
+    # Menulis setiap baris data
+    for review in scrapreview:
+        writer.writerow(review.values())
